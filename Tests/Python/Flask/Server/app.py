2f2f9d660ab3093b8136546c6c9418b69c0c04e0
tfshaman <tfshaman@gmail.com>
2019-05-19 01:37:03 +0200

    Local ip
    now running on the local ip of device

--- Tests/Python/Flask/Server/app.py
+++ Tests/Python/Flask/Server/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+localIp = (s.getsockname()[0])
+s.close()
 
 app = Flask(__name__, static_url_path='')
+
+
+
 
 @app.route('/')
 def index():
@@ -8,4 +17,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.5',debug=True)
+    app.run(host=localIp,debug=True)
